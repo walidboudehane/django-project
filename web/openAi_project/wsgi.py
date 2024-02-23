944b32ac77ad02442725ab245f0691080202dea1
Walid <walid.boudehane@umontreal.ca>
2024-02-23 18:34:19 -0500

    init project

---
+++ web/openAi_project/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for openAi_project project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'openAi_project.settings')
+
+application = get_wsgi_application()
